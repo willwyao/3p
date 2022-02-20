a511c837f2b1ebc0132f39cf7ab1061b6c1013ab
WEI YAO <WillYao@WEIdeMBP.modem>
2022-02-20 17:50:05 +1100

    setup initial state and start page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import './App.css';
+import "./App.css";
+import quizzesJSON from "./data/quizzes.json";
+import questionsJSON from "./data/questions.json";
+import React from "react";
+import Start from "./pages/Start";
+import Finish from "./pages/Finish";
+import Quiz from "./pages/Quiz";
+
+const initState = { progress: -1, answers: {}, scores: {} };
+const quizzes = quizzesJSON.quizzes;
+const questionsObj = {};
+questionsJSON.questions.forEach((question) => {
+  questionsObj[question.id] = question;
+});
 
 function App() {
+  const [record, setRecord] = React.useState(
+    JSON.parse(localStorage.getItem("record")) || initState
+  );
+  const progress = record.progress;
+  const amount = quizzes.length;
+
+  React.useEffect(() => {
+    if (localStorage.getItem("record") === null) {
+      localStorage.setItem("record", JSON.stringify(initState));
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {progress === -1 ? (
+        <Start record={record} setRecord={setRecord} />
+      ) : progress === amount ? (
+        <Finish />
+      ) : (
+        <Quiz />
+      )}
     </div>
   );
 }
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Start = () => {
+const Start = ({ record, setRecord }) => {
+  const nextPage = () => {
+    const newRecord = { ...record, progress: record.progress + 1 };
+    localStorage.setItem("record", JSON.stringify(newRecord));
+    setRecord(newRecord);
+  };
   return (
-    <div>Start</div>
-  )
-}
+    <div className="container">
+      <h1>Assessment</h1>
+      <p>Click the button to start the assessment.</p>
+      <button className="btn btn-primary" onClick={nextPage}>
+        Start
+      </button>
+    </div>
+  );
+};
 
-export default Start
+export default Start;
